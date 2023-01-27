55c7bf2dd0885bcf554f0a162537328c44336c0e
LamCode47 <mohamedlamine.silarbi@gmail.com>
2023-01-27 16:08:41 +0100

    versionB

--- script.js
+++ script.js
@@ -6,37 +6,27 @@ let links = document.querySelectorAll("ul a");
 let menu = document.querySelector("nav ul")
 let svg = document.querySelector(".svg")
 let span = document.querySelector(".spans")
+let body = document.querySelector("body")
 
-links.forEach(function(elem) {
-    elem.addEventListener("click", function() {
-        menu.style.left = "-6cm";
-        span.style.display = "flex"
-        svg.style.display = "none"
-    });
-});
-
-svg.addEventListener("click", () => {
-    menu.style.left = "-6cm";
-    span.style.display = "flex"
-    svg.style.display = "none"
-} )
-console.log(links)
+// open menu nav
 function btn() {
 
     menu.style.left = "0cm";
-    svg.style.left = "6.2cm";
+    svg.style.left = "6cm";
     span.style.display = "none"
     svg.style.display = "block"
 
 }
 
+// Click close menu
 
-// -------------------------- jquery scrit----------//
+let closeMenu = () => {
+            menu.style.left = "-6cm";
+            span.style.display = "flex"
+            svg.style.display = "none"
+        };
 
-// $("button").on("click" , function () {
-//     $("nav ul").toggleClass("0cm 6.6cm")
-//     menu.style.left = "0cm";
-//     button.style.marginLeft = "6.5cm";
-//     svg.style.display = "block"
-//     span.style.display = "none"
-// });
+links.forEach((e) => {
+    e.addEventListener("click", closeMenu)
+});
+svg.addEventListener("click", closeMenu);
