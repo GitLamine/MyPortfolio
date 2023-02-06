cc06ddf7ea00515fd317de2e159bb1f5697efb77
LamCode47 <mohamedlamine.silarbi@gmail.com>
2023-02-06 10:04:42 +0100

    version02

---
+++ API.php
@@ -0,0 +1,10 @@
+<?php
+$key = "2052c63b9ef7bf04d754096eef437dbb";
+$url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=$key";
+
+$city = "paris";
+echo "$url"
+
+
+
+?>
