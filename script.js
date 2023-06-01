a74162258ec1fb25fb9336a50dc307e43694f019
LamCode47 <mohamedlamine.sialarbi@gmail.com>
2023-06-01 16:29:38 +0200

    fix bur burger menu

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-
-
 // -------------------js nativ script------------------//
 
 let links = document.querySelectorAll("ul a");
 let menu = document.querySelector("nav ul")
 let li = document.querySelectorAll('li')
 let svg = document.querySelector(".svg")
-let span = document.querySelector(".spans")
+let btnMenu = document.querySelector(".spans")
 let body = document.querySelector("body")
 
 // Click open menu
@@ -14,24 +12,27 @@ function btn() {
 
     menu.style.left = "0cm";
     svg.style.left = "6cm";
-    span.style.display = "none"
+    btnMenu.style.display = "none"
     svg.style.display = "block"
-
-
 }
 
 // var close menu
-
 let closeMenu = () => {
-            menu.style.left = "-6cm";
-            menu.style.overflow = "hidden"
-            span.style.display = "flex"
-            svg.style.display = "none"
-            li.style.width = "100%"
-        };
-
+    menu.style.left = "-6cm";
+    menu.style.overflow = "hidden";
+    btnMenu.style.display = "flex";
+    svg.style.display = "none";
+    li.forEach((item) => {
+        item.style.width = "100%";
+    });
+};
 links.forEach((e) => {
     e.addEventListener("click", closeMenu)
 });
 // hide 
 svg.addEventListener("click", closeMenu);
+body.addEventListener("click", function (e) {
+    if (e.target != btnMenu && e.target != svg) {
+        closeMenu();
+    }
+});
