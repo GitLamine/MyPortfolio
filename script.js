5fb2d54c3c7e6c57e62d93b9ee79cc522a27c531
LamCode47 <mohamedlamine.sialarbi@gmail.com>
2023-06-12 17:27:03 +0200

    burger_menu_fixed

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
-// -------------------js nativ script------------------//
 
+// -------------------js nativ script------------------//
 let links = document.querySelectorAll("ul a");
 let menu = document.querySelector("nav ul")
 let li = document.querySelectorAll('li')
 let svg = document.querySelector(".svg")
-let btnMenu = document.querySelector(".spans")
-let body = document.querySelector("body")
+let span = document.querySelector(".spans")
+
 
 // Click open menu
 function btn() {
-
+    menu.style.display = "flex";
     menu.style.left = "0cm";
     svg.style.left = "6cm";
-    btnMenu.style.display = "none"
+    span.style.display = "none"
     svg.style.display = "block"
 }
 
 // var close menu
 let closeMenu = () => {
     menu.style.left = "-6cm";
-    menu.style.overflow = "hidden";
-    btnMenu.style.display = "flex";
-    svg.style.display = "none";
-    li.forEach((item) => {
-        item.style.width = "100%";
-    });
+    span.style.display = "flex"
+    svg.style.display = "none"
+    li.style.width = "100%"
 };
 links.forEach((e) => {
     e.addEventListener("click", closeMenu)
 });
 // hide 
 svg.addEventListener("click", closeMenu);
-body.addEventListener("click", function (e) {
-    if (e.target != btnMenu && e.target != svg) {
-        closeMenu();
-    }
-});
